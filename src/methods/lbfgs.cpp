9694e61ae0d641399c039f1b459574e7fec8d8e7
CHARILOG <charilog@users.noreply.github.com>
2026-05-23 13:43:46 +0300

    Delete src/methods directory

--- src/methods/lbfgs.cpp
+++
@@ -1,9 +0,0 @@
-#include "lbfgs.h"
-
-namespace optimsolution {
-
-void LBFGS::configure(const MethodConfig&) { /* no-op stub */ }
-void LBFGS::init()                          { /* no-op stub */ }
-void LBFGS::one_iteration()                 { /* no-op stub */ }
-
-} // namespace optimsolution
--- src/methods/nm.cpp
+++
@@ -1,9 +0,0 @@
-#include "nm.h"
-
-namespace optimsolution {
-
-void NM::configure(const MethodConfig&) { /* no-op stub */ }
-void NM::init()                         { /* no-op stub */ }
-void NM::one_iteration()                { /* no-op stub */ }
-
-} // namespace optimsolution
