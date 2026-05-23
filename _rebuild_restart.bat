b1943fa6a1fc94bc1fccaa7732f0cd6ee951a5a6
CHARILOG <charilog@users.noreply.github.com>
2026-05-23 13:31:29 +0300

    Add files via upload

---
+++ _rebuild_restart.bat
@@ -0,0 +1,8 @@
+@echo off
+timeout /t 2 /nobreak > nul
+cd /d "C:\Users\admin\Desktop\optimsolution"
+cmake --build build --config Release
+if %ERRORLEVEL% neq 0 (
+    echo Rebuild needed > "C:\Users\admin\Desktop\optimsolution\.rebuild_pending"
+)
+start "" "C:\Users\admin\Desktop\optimsolution\build\Release\optimsolution_gui.exe" C:\Users\admin\Desktop\optimsolution\build\Release\optimsolution_gui.exe
